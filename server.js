cf0c15ddbb164bc9c26a6c96f5620c261b00c836
Darkst0rm-1 <melghara@my.centennialcollege.ca>
2025-10-14 19:21:22 -0400

    upgrade

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var express = require('express');
 var createError = require('http-errors');
 var logger = require('morgan');
@@ -46,3 +47,16 @@ app.listen(3000);
 console.log('Server running at http://localhost:3000/');
 
 module.exports = app;
+=======
+const http = require('http');
+
+http.createServer((req, res) => {
+res.writeHead(200, {
+'Content-Type': 'text/plain'
+});
+
+res.end('Hello World');
+}).listen(3000);
+
+console.log('Server running at http://localhost:3000/');
+>>>>>>> Stashed changes
